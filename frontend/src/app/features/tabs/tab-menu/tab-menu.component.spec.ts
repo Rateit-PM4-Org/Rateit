a9e45b0a47629e43674f0237a75ebaa3ae1505b0
michamettler <mettlermicha@gmail.com>
2025-03-23 19:06:33 +0100

    added tests for menubar

--- frontend/src/app/features/tabs/tab-menu/tab-menu.component.spec.ts
+++ frontend/src/app/features/tabs/tab-menu/tab-menu.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { provideHttpClient } from '@angular/common/http';
@@ -21,6 +21,27 @@ describe('TabMenuComponent', () => {
     const fixture = TestBed.createComponent(TabMenuComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should render two tab buttons', () => {
+    const fixture = TestBed.createComponent(TabMenuComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    const tabButtons = compiled.querySelectorAll('ion-tab-button');
+    expect(tabButtons.length).toBeGreaterThanOrEqual(2);
+  });
+
+  it('should have a "home" tab', () => {
+    const fixture = TestBed.createComponent(TabMenuComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    const homeTab = compiled.querySelector('ion-tab-button[tab="home"]');
+    expect(homeTab).toBeTruthy();
+  });
+
+  it('should have a "profile" tab', () => {
+    const fixture = TestBed.createComponent(TabMenuComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    const profileTab = compiled.querySelector('ion-tab-button[tab="profile"]');
+    expect(profileTab).toBeTruthy();
   });
 
 });
