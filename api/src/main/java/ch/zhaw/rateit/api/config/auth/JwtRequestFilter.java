f01bef19b3a11e7780cb742dfd46f0fa9cbbd821
averel10 <achille.huenenberger@gmail.com>
2025-03-27 17:52:38 +0100

    Enhance JWT authentication handling with improved error management

--- api/src/main/java/ch/zhaw/rateit/api/config/auth/JwtRequestFilter.java
+++ api/src/main/java/ch/zhaw/rateit/api/config/auth/JwtRequestFilter.java
@@ -42,23 +42,27 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             return;
         }
 
+        try {
+            final String jwt = authHeader.substring(7);
+            final String userEmail = jwtService.extractSubject(jwt);
 
-        final String jwt = authHeader.substring(7);
-        final String userEmail = jwtService.extractSubject(jwt);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (userEmail != null && authentication == null) {
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
 
-        if (userEmail != null && authentication == null) {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+                if (jwtService.isTokenValid(jwt, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 
-            if (jwtService.isTokenValid(jwt, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
             }
-        }
 
+
+        } catch (RuntimeException ignored) {
+
+        }
         filterChain.doFilter(request, response);
     }
 }
