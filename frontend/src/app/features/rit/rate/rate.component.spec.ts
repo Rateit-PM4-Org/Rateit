8098279885f73562140461548eb4feb69ee82339
kuengpas <pascal-kueng@gmx.net>
2025-03-28 11:26:20 +0100

    set up base folder structure for rate feature

---
+++ frontend/src/app/features/rit/rate/rate.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { RateComponent } from './rate.component';
+
+describe('RateComponent', () => {
+  let component: RateComponent;
+  let fixture: ComponentFixture<RateComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [RateComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(RateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/features/rit/rate/rate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rate',
+  templateUrl: './rate.component.html',
+  styleUrls: ['./rate.component.scss'],
+  standalone: true,
+})
+export class RateComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
