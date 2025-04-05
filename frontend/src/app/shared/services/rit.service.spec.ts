0248825b6b03a0ef3fe53d26884cd93a8a4590bc
mettlmi1 <mettlermicha@gmail.com>
2025-04-05 08:56:54 +0200

    added test for ritService

--- frontend/src/app/shared/services/rit.service.spec.ts
+++ frontend/src/app/shared/services/rit.service.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import { provideMockApiService, provideMockAuthService } from '../test-util/test-util';
 import { RitService } from './rit.service';
+import { ApiService } from './api.service';
+import { first, of } from 'rxjs';
+import { Rit } from '../../model/rit';
 
 describe('RitService', () => {
   let service: RitService;
@@ -18,6 +21,17 @@ describe('RitService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should create a new rit', () => {
+      const apiServiceSpy = (TestBed.inject(ApiService)) as jasmine.SpyObj<ApiService>;
+      apiServiceSpy.post.and.returnValue(of({ success: true }));
+      const mockResponse = { success: true };
+      const rit: Rit = { name: 'testRit', details: 'some details', tags: ['tag1', 'tag2'] };
+  
+      service.createRit(rit).pipe(first()).subscribe(response => {
+        expect(response).toEqual(mockResponse);
+      });
+    });
 
 
 });
