1d948ef008ba1a3d5c0c8ccccc9c5fe89da6ae38
averel10 <achille.huenenberger@gmail.com>
2025-04-14 21:00:46 +0200

    fixed tests

--- frontend/src/app/features/rating/rating-list-item/rating-list-item.component.spec.ts
+++ frontend/src/app/features/rating/rating-list-item/rating-list-item.component.spec.ts
@@ -30,12 +30,10 @@ describe('RatingListItemComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render rating title, positive and negative comment', () => {
-    const titleElement = fixture.debugElement.query(By.css('.title')).nativeElement;
+  it('should render positive and negative comment', () => {
     const positiveComment = fixture.debugElement.query(By.css('.rating-positive ion-text')).nativeElement;
     const negativeComment = fixture.debugElement.query(By.css('.rating-negative ion-text')).nativeElement;
 
-    expect(titleElement.textContent).toContain(testRating.value);
     expect(positiveComment.textContent).toContain(testRating.positiveComment);
     expect(negativeComment.textContent).toContain(testRating.negativeComment);
   });
