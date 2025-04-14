f6346bc3a2afcbc8cefef256360abccede6144c2
averel10 <achille.huenenberger@gmail.com>
2025-04-14 14:26:50 +0200

    sonar issue

--- frontend/src/app/features/rit/rit-list-item/rit-list-item.component.ts
+++ frontend/src/app/features/rit/rit-list-item/rit-list-item.component.ts
@@ -29,7 +29,7 @@ export class RitListItemComponent {
     }
     const latestRating = ratings.reduce((prev, current) => {
       return new Date(prev.createdAt!) > new Date(current.createdAt!) ? prev : current;
-    });
+    }, ratings[0]);
 
     return latestRating.value ?? 0;
   }
