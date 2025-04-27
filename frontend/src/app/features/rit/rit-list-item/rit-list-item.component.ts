cead353489e7a180b1d740d88efc6b50c1eb34d9
bermic01 <bermic01@students.zhaw.ch>
2025-04-27 23:17:30 +0200

    implemented deleteRit in frontend

--- frontend/src/app/features/rit/rit-list-item/rit-list-item.component.ts
+++ frontend/src/app/features/rit/rit-list-item/rit-list-item.component.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Rating } from '../../../model/rating';
+import {RitService} from '../../../shared/services/rit.service';
 import { Rit } from '../../../model/rit';
 import { IonicStandaloneStandardImports } from '../../../shared/ionic-imports';
 import { NavController } from '@ionic/angular';
+import {ToastController} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-rit-list-item',
@@ -19,7 +21,12 @@ export class RitListItemComponent {
 
   protected latestRatingValue: number = 0;
 
-  constructor(private readonly router: Router, private readonly navCtrl: NavController) {
+
+  constructor(private readonly router: Router,
+              private readonly navCtrl: NavController,
+              private readonly ritService: RitService,
+              private readonly toastController: ToastController,
+              ) {
   }
 
   ngOnInit() {
@@ -60,6 +67,61 @@ export class RitListItemComponent {
       queryParams: { tag: tagName }
     });
     event.stopPropagation();
+  }
+  private loadRits(): void {
+    this.ritService.getRits().subscribe({
+      next: (rits) => {
+        this.handleSuccess(rits);
+      },
+      error: (err: any) => {
+        this.handleError(err);
+      }
+    });
+  }
+  //handle success withount triggering reload
+  private handleSuccess(data: Rit[]) {
+
+  }
+
+  private handleError(err: any) {
+    const baseError = err.error?.error ?? 'Unknown error';
+    this.showErrorToast(baseError);
+  }
+  async showErrorToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'top',
+      color: 'danger',
+    });
+
+    await toast.present();
+  }
+
+
+  async showSuccessToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'top',
+      color: 'success',
+    });
+
+    await toast.present();
+  }
+
+  deleteRit(ritId: string | undefined): void {
+    this.ritService.deleteRit(ritId).subscribe({
+      next: () => {
+        this.showSuccessToast('Rit deleted successfully!');
+        this.ritService.triggerRitsReload().subscribe(() => {
+          this.loadRits();
+        });
+      },
+      error: (err: any) => {
+        this.handleError(err);
+      }
+    });
   }
 
 }
