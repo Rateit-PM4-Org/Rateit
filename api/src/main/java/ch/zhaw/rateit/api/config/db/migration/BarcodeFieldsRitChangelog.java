e574cee87846b80e9003af54bac12642c8e28f19
zillinic <nicolas.zillig@gmx.ch>
2025-04-29 20:33:04 +0200

    added db migration for barcodes

---
+++ api/src/main/java/ch/zhaw/rateit/api/config/db/migration/BarcodeFieldsRitChangelog.java
@@ -0,0 +1,31 @@
+package ch.zhaw.rateit.api.config.db.migration;
+
+import ch.zhaw.rateit.api.logic.rit.repository.RitRepository;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+
+import java.util.List;
+
+/**
+ * Neue Felder für Barcodes auf Rits:
+ * - auf vorhandenen Rits leere Barcode List hinzufügen
+ *
+ * @author Nicolas Zillig
+ */
+@ChangeUnit(id = "barcodeFieldsRitChangelog", order = "002", author = "Nicolas Zillig")
+public class BarcodeFieldsRitChangelog {
+    @Execution
+    public void addBarcodeFields(RitRepository ritRepository) {
+        // Add barcode fields to rits
+        ritRepository.findAll().forEach(rit -> {
+            rit.setCodes(List.of());
+            ritRepository.save(rit);
+        });
+    }
+
+    @RollbackExecution
+    public void rollbackVerificationFields() {
+        // not reversable
+    }
+}
