32e7c2486d0f7b66e02b20dc588bee66173b3765
SamKry <87971518+SamKry@users.noreply.github.com>
2025-05-02 09:17:10 +0200

    add import and remove unused comment

--- frontend/src/app/shared/ionic-imports.ts
+++ frontend/src/app/shared/ionic-imports.ts
@@ -33,6 +33,7 @@ import {
   IonSegment,
   IonSegmentButton,
   IonSegmentContent,
+  IonSelectOption,
   IonSpinner,
   IonTabBar,
   IonTabButton,
@@ -93,6 +94,6 @@ export const IonicStandaloneStandardImports = [
   IonRefresherContent,
   IonItemSliding,
   IonItemOptions,
-  IonItemOption
-
+  IonItemOption,
+  IonSelectOption,
 ];
