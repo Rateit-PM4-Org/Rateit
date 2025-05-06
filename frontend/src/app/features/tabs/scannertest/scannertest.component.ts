bf77351eb87fc8bba1d9fe5134e5c1a6733abd74
averel10 <achille.huenenberger@gmail.com>
2025-05-06 15:32:48 +0200

    added dynamic width for component (with max 640px)

--- frontend/src/app/features/tabs/scannertest/scannertest.component.ts
+++ frontend/src/app/features/tabs/scannertest/scannertest.component.ts
@@ -2,15 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { IonicStandaloneStandardImports } from '../../../shared/ionic-imports';
 import { ScannerComponent } from '../../scanner/scanner.component';
 import { ToastController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-scannertest',
   templateUrl: './scannertest.component.html',
   styleUrls: ['./scannertest.component.scss'],
   standalone: true,
-  imports: [...IonicStandaloneStandardImports, ScannerComponent],
+  imports: [...IonicStandaloneStandardImports, ScannerComponent, CommonModule],
 })
 export class ScannertestComponent  implements OnInit {
+
+  protected scannedCodes: Array<string> = new Array<string>();
 
   constructor(private toastController: ToastController) { }
 
@@ -18,6 +21,9 @@ export class ScannertestComponent  implements OnInit {
 
   async onScanned(scannedCodes: Set<string>) {
     const message = Array.from(scannedCodes).join(', ');
+    scannedCodes.forEach((code) => {
+      this.scannedCodes.push(code);
+    })
 
     const toast = await this.toastController.create({
       message: `Scanned codes: ${message}`,
