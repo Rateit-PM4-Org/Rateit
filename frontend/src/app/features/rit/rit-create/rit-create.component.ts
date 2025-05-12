5d680abcc9600929d81d2b468abec69fea447c1b
averel10 <achille.huenenberger@gmail.com>
2025-05-12 14:27:58 +0200

    removed automatic updating of rit

--- frontend/src/app/features/rit/rit-create/rit-create.component.ts
+++ frontend/src/app/features/rit/rit-create/rit-create.component.ts
@@ -162,7 +162,7 @@ export class RitCreateComponent implements ViewWillEnter, ModalContent {
     this.ritUpdateModal.modal.present();
     const {data} = await this.ritUpdateModal.modal.onDidDismiss();
     this.addCodes(data.scannedCodes);
-    this.updateRit();
+    //this.updateRit();
   }
 
   setRitName(event: any) {
